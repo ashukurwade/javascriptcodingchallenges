b63ed1fa7ea19123edc7a641bf234c21a0fcee9f
Ashutosh Kurwade <ashutoshkurwade@gmail.com>
2023-01-12 01:58:36 +0530

    return true if the number is in the array otherwise false

---
+++ Find a number in Array/findaNumberinArray.js
@@ -0,0 +1,15 @@
+// write a function which finds a number in an array of number
+// return true if the number is in the array otherwise false
+
+function findNumInArray(num, numbers){
+    for(let i=0; i<numbers.lenght; i++){
+        if( num == numbers[i]){
+            return true;
+
+        }
+        
+    }
+    return false;
+}
+
+console.log(findNumInArray(5, [8,9,7,6,4,5,3,2,]))
