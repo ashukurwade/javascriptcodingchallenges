812279bc52ed79dd15757efeb0e46780f53ed52a
Ashutosh Kurwade <ashutoshkurwade@gmail.com>
2023-01-12 01:59:24 +0530

    function which takes a number and prints the table

---
+++ Print Table/printTable.js
@@ -0,0 +1,12 @@
+// write a function which takes a number and prints the table
+// 5 * 1 =5
+// 5 * 2 =10
+
+function printTable(num){
+    for(let i=1; i<=10; i++){
+        console.log('${num} * ${i} = ${num*i}');
+
+    }
+}
+
+printTable(8)
